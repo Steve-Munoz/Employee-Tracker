fa95bd7f99ee3e4b5e9b7f75ee9ac6a50263ad0a
Steve Munoz <stevomunoz@yahoo.com>
2020-07-12 18:53:53 -0500

    deleted comments and code that was moved to another folder/file

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
 // ↓ This is a Node.js module available thorugh the npm registry. Need to run " npm install mysql" to install mysql dependency
-const mysql = require("mysql");
 const { start } = require("./inquirer");
 const { connection } = require("./db/index.js");
 // const {start} = require("./prompt");
 
-// ↓ creates the connection information for the sql database.
-// var connection = mysql.createConnection({
-//   // ↓ below are my connection options. See https://www.npmjs.com/package/mysql for more info
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "employee_TrackerDB",
-// });
-// ↓ connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err; //this mean connection failed!!
 
